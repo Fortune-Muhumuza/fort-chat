82458463bde7f41902fae42ff605d894da2e507b
Fortune <fortunemuhumuza@gmail.com>
2021-07-12 19:42:25 -0700

    fetch users from api on start of app

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import './index.css'
 import App from './App'
 import store from './store/index'
 import { Provider } from 'react-redux'
+import { fetchUsers } from './store/reducers/users/usersSlice'
 
 import './api/server'
+
+store.dispatch(fetchUsers())
 
 ReactDOM.render(
   <React.StrictMode>
